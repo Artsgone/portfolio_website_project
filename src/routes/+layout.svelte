e1c6b7bf62c32c70efb76bf78b55940c3333d66c
Artsgone <daminart04@gmail.com>
2025-06-22 22:11:13 +0200

    update to layout svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { onMount } from 'svelte';
+  import { afterNavigate } from '$app/navigation';
+
+  const GA_ID = 'G-BL8ZL12VTN';
+
+  onMount(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=G-BL8ZL12VTN`;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){ window.dataLayer.push(arguments); }
+    window.gtag = gtag;
+
+    gtag('js', new Date());
+    gtag('config', GA_ID, { page_path: window.location.pathname });
+
+    afterNavigate(() => {
+      gtag('config', GA_ID, { page_path: window.location.pathname });
+    });
+  });
+</script>
+
+<slot />
